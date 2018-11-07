6cefe48bacf8455bd02ba8c6b8f84b15c6883593
Eric Johnson <ericj@singledigit.net>
2018-11-07 16:21:17 -0700

    rough code for rekognition

--- process-service/app.js
+++ process-service/app.js
@@ -1,30 +1,34 @@
 const AWS = require('aws-sdk');
 const rek = new AWS.Rekognition();
+const dc = new AWS.DynamoDB.DocumentClient();
 
 
 exports.lambdaHandler = async (event) => {
-  let faceDetectionPromises = []
+  let faceDetectionPromises = [];
+  let messageIds = [];
+  let receiptHandles = [];
+  let imageIds = [];
+  let keys = [];
 
   // Build array of images to process
-  let images = event.Records.map(record => {
-    return {
-      bucket: record.s3.bucket.name,
-      key: record.s3.object.key,
-    }
-  })
+  event.Records.map(record => {
+    messageIds.push(record.messageId);
+    receiptHandles.push(record.receiptHandle);
+    JSON.parse(record.body).Records.map(inner => {
+      imageIds.push(inner.s3.object.eTag);
+      keys.push(inner.s3.object.key);
+      let params = {
+        Image: {
+          S3Object: {
+            Bucket: inner.s3.bucket.name,
+            Name: inner.s3.object.key,
+          }
+        },
+        Attributes: ['ALL']
+      }
 
-  images.map(image => {
-    let params = {
-      Image: {
-        S3Object: {
-          Bucket: images[0].bucket,
-          Name: images[0].key,
-        }
-      },
-      Attributes: ['ALL']
-    }
-
-    faceDetectionPromises.push(rek.detectFaces(params).promise());
+      faceDetectionPromises.push(rek.detectFaces(params).promise());
+    })
   })
 
   let detectFacesData = await Promise.all(faceDetectionPromises).catch(err => {
@@ -33,7 +37,28 @@ exports.lambdaHandler = async (event) => {
     return err.stack;
   })
 
+  
   // push to dynamodb.
+  let putParams = {
+    RequestItems: {}
+  }
+  putParams.RequestItems[process.env.TABLE_NAME] = detectFacesData.map((face, i) => {
+    return {
+      PutRequest: {
+        Item: {
+          id: `${keys[i]}-${imageIds[i]}`,
+          type: "detectFace",
+          data: face.FaceDetails
+        }
+      }
+    }
+  })
 
-  return detectFacesData;
+  await dc.batchWrite(putParams).promise().catch(err => {
+    console.log("Error Saving Data to Dynamo");
+    console.log(err.stack);
+    return err.stack;
+  })
+
+  return {success: true};
 };
