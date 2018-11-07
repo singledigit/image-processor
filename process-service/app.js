15e65ada75b17a561e5f8571f00a2e15ade621b7
Eric Johnson <ericj@singledigit.net>
2018-11-07 10:05:55 -0700

    looking back to sqs

--- process-service/app.js
+++ process-service/app.js
@@ -1,8 +1,8 @@
 exports.lambdaHandler = (event, context, callback) => {
   let images = event.Records.map(record => {
     return {
-      bucket: record.body.Records[0].s3.bucket.name,
-      key: record.body.Records[0].s3.object.key
+      bucket: record.s3.bucket.name,
+      key: record.s3.object.key,
     }
   })
 
