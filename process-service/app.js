28ab14a8fd8c645040da55aa035c59847249979d
Eric Johnson <ericj@singledigit.net>
2018-11-07 14:02:30 -0700

    Full rekognition call

--- process-service/app.js
+++ process-service/app.js
@@ -1,4 +1,11 @@
-exports.lambdaHandler = (event, context, callback) => {
+const AWS = require('aws-sdk');
+const rek = new AWS.Rekognition();
+
+
+exports.lambdaHandler = async (event) => {
+  let faceDetectionPromises = []
+
+  // Build array of images to process
   let images = event.Records.map(record => {
     return {
       bucket: record.body.Records[0].s3.bucket.name,
@@ -6,5 +13,27 @@ exports.lambdaHandler = (event, context, callback) => {
     }
   })
 
-  callback(null, images);
+  images.map(image => {
+    let params = {
+      Image: {
+        S3Object: {
+          Bucket: images[0].bucket,
+          Name: images[0].key,
+        }
+      },
+      Attributes: ['ALL']
+    }
+
+    faceDetectionPromises.push(rek.detectFaces(params).promise());
+  })
+
+  let detectFacesData = await Promise.all(faceDetectionPromises).catch(err => {
+    console.log("Error Detecting Faces");
+    Console.log(err.stack);
+    return err.stack;
+  })
+
+  // push to dynamodb.
+
+  return detectFacesData;
 };
